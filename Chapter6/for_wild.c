93ee72be33ab380e439350f3803fa28fa85fdfe4
dvogureckiy99 <dvogureckiy99@gmail.com>
2022-08-15 21:39:28 +0300

    new

--- Chapter6/for_wild.c
+++ Chapter6/for_wild.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 int main(void)
 {
-    for (int x = 1,y = 55; y <= 375; y = (++x * 5) + 50*x^2)
+    for (int x = 1,y = 55; y <= 375; y = (++x * 5) + 13*x^2)
     printf("%10d %10d\n", x, y);
 
     return 0;
