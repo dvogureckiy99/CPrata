823651ca1672b37b8bc822743d9f5464d5d6286a
dvogureckiy99 <dvogureckiy99@gmail.com>
2022-08-19 22:30:17 +0300

    new

--- first.c
+++ first.c
@@ -10,7 +10,7 @@ num = 1;
 */
 printf("I am a simple "); /* use the printf() function
 */
-printf("computer.\n");
+printf("computer. \n");
 printf("My favorite number is %d because it is first.\n",num);
 return 0;
 }
