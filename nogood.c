af39a521c5daafa417b8d824adca7bbc9af68f5b
Dmitriy Ogureckiy <dvogureckiy99@gmail.com>
2022-08-21 14:05:35 +0300

    new

--- nogood.c
+++ nogood.c
@@ -2,11 +2,11 @@
 #include <stdio.h>
 int main(void)
 {
-int n, n2, n3;
-// this program has several errors
-n = 5;
-n2 = n * n;
-n3 = n2 * n;
-printf("n = %d, n squared = %d, n cubed = %d\n", n, n2, n3);
-return 0;
+  int n, n2, n3;
+  // this program has several errors
+  n = 5;
+  n2 = n * n;
+  n3 = n2 * n;
+  printf("n = %d, n squared = %d, n cubed = %d\n", n, n2, n3);
+  return 0;
 }
