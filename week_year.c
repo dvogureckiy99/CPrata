aa1ed2257eb6dbcdc4285eaa7849516953d2fb07
Dmitriy Ogureckiy <dvogureckiy99@gmail.com>
2022-08-23 20:18:09 +0300

    new

--- week_year.c
+++ week_year.c
@@ -3,10 +3,10 @@
 int main(void) /* this prints the number of weeks in a year */ 
 
 { 
-int words = 3020; 
+    int words = 3020; 
 
-int lines = 350; 
-printf("There were %d words and %d lines.\nHere, %d and %d represent the values of the two veriables.",words,lines,words,lines); 
+    int lines = 350; 
+    printf("There were %d words and %d lines.\nHere, %d and %d represent the values of the two veriables.",words,lines,words,lines); 
 
     return 0; 
 
