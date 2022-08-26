f9f73ea1562e51000f0002913f2f6815d5b677ad
Dmitriy Ogureckiy <dvogureckiy99@gmail.com>
2022-08-26 19:34:25 +0300

    Update summing.c

--- Chapter6/summing.c
+++ Chapter6/summing.c
@@ -6,7 +6,7 @@ int main(void)
     long sum = 0L;     /* initialize sum to zero    */
     int status;
 
-    printf("Please enter an integer to be summed ");
+    printf("Please enter an integer to be summed:");
     printf(" (q to quit): ");
     status = scanf("%ld", &num);
     while (status == 1) /* == means "is equal to"
