3ed3f59af917d87824a9031ae246a7c1ec6ad742
Dmitriy Ogureckiy <dvogureckiy99@gmail.com>
2022-08-27 10:47:08 +0300

    Update escape.c

--- Chapter3/escape.c
+++ Chapter3/escape.c
@@ -4,7 +4,7 @@ int main(void)
 {
     float salary;
     printf("\aEnter your desired monthly salary:");/* 1 */
-    printf(" $_______\b\b\b\b\b\b\b");
+    printf(" $____\b\b\b\b\b\b\b");
     /* 2 */
     scanf("%f", &salary);
     printf("\n\t$%.2f a month is $%.2f a year.", salary,
