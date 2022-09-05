7bd7ef47863d03ef5cc9a41713ae9b611950c075
Dmitriy Ogureckiy <dvogureckiy99@gmail.com>
2022-09-05 19:21:29 +0300

    Update review5.c

--- Chapter3/review5.c
+++ Chapter3/review5.c
@@ -1,8 +1,8 @@
     #include <stdio.h>
     int main ()
     {
-    double tax, rate, g;
-    g = 1.e21;
-    tax = rate*g;
-    return 0;
+        double tax, rate, g;
+        g = 1.e21;
+        tax = rate*g;
+        return 0;
     }
