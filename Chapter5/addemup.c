5b5e0c98f9c3a7807c0d26e5549e2acb54fff35e
Dmitriy Ogureckiy <dvogureckiy99@gmail.com>
2022-09-10 11:22:13 +0300

    Update addemup.c

--- Chapter5/addemup.c
+++ Chapter5/addemup.c
@@ -3,12 +3,12 @@
 int main(void)
 /* finds sum of first 20 integers */
 {
-int count, sum; /* declaration statement */
-count = 0; 
-sum = 0;
-while (count++ < 20)
-    sum = sum + count;
-printf("sum = %d\n", sum);
+    int count, sum; /* declaration statement */
+    count = 0; 
+    sum = 0;
+    while (count++ < 20)
+        sum = sum + count;
+    printf("sum = %d\n", sum);
 
-return 0;
+    return 0;
 }
